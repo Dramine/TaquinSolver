03259849bff70b46b519e6ede2bc1e905d327a7d
amine <amine.boujemt@etu.univ-lyon1.fr>
2021-06-28 18:32:36 +0200

    ajout de l'affichage graphique

---
+++ displayer.py
@@ -0,0 +1,32 @@
+from tkinter import *
+from PIL import Image, ImageTk
+import tkinter as tk
+from environment import Environment
+from utils import PositionManager
+
+
+class Application(tk.Tk):
+    def __init__(self, env: Environment):
+        super().__init__()
+        self.env = env
+        self.width = 500
+        self.height = 500
+        self.create_window()
+        self.canvas_grid = []
+        self.images = []
+        self.load_image()
+
+    def create_window(self):
+        self.title("The taquin")
+        self.geometry(str(self.width + self.env.n*4)+"x"+str(self.height + self.env.n*4)+"+0+0")
+
+    def load_image(self):
+        for pos, agent in list(self.env.agents.items()):
+            i = agent.id
+            pos = PositionManager.pos_2D(pos, self.env.n)
+            img = ImageTk.PhotoImage(Image.open("images/" + str(self.env.n)+"x"+str(self.env.n)+"/"+str(i) + ".png"))
+            self.images.append(img)
+            self.canvas_grid.append(Canvas(self, width=self.width/self.env.n, height=self.height/self.env.n))
+            self.canvas_grid[i].grid(row=pos[1], column=pos[0])
+            self.canvas_grid[i].create_image(0, 0, anchor=NW, image=self.images[i])
+        self.after(500, self.load_image)
