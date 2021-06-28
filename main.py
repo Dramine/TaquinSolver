f856ee4f8d354a9823e93921131ec7c2afede9e9
amine <amine.boujemt@etu.univ-lyon1.fr>
2021-06-28 18:32:05 +0200

    modif main affichage

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 from environment import Environment
 from agents import SimpleAgent, DijkstraAgent, InteractiveDijkstraAgent, Messenger, ACKMessage, GiveWayMessage
+from displayer import Application
+from PIL import Image, ImageTk
 
 N = 5
 
 if __name__ == '__main__':
     env = Environment(N)
-    for i in range(10):
-        # env.add_agent(SimpleAgent(i, env.generate_random_position(), i, env))
-        # env.add_agent(DijkstraAgent(i, env.generate_random_position(), i, env))
-        env.add_agent(InteractiveDijkstraAgent(i, env.generate_random_position(), i, env))
+    for i in range(7):
+        #env.add_agent(SimpleAgent(i, env.generate_random_position(), i, env))
+        env.add_agent(DijkstraAgent(i, env.generate_random_position(), i, env))
+        #env.add_agent(InteractiveDijkstraAgent(i, env.generate_random_position(), i, env))
 
+    app = Application(env)
     env.start_agent()
-    while not env.is_finish():
-        if env.update:
-            print(env)
-            env.update = False
-    print(env)
+    app.mainloop()
+
+# for i in range(0, 17):
+#     im = Image.open("images/4x4/"+str(i)+".png")
+#     im2 = im.resize((125,125))
+#     im2.save(str(i)+".png")
